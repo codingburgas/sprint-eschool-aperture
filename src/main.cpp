e47c85973a48fdd041b0e34d1d77e01ed320d3ed
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2025-03-07 22:07:17 +0200

    Remove pointer declaration of class in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
-#include <raylib.h>
 #include <app.hpp>
-#include <utilities.hpp>
 
 int main()
 {
-	App* app = new App({ 1440, 800 }, "Aperture Notepad");
-	app->Run();
-
-	delete app;
+	App app = App({ 1440, 800 }, "Aperture Notepad");
+	app.Run();
 }
