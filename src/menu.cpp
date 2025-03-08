7296e0a796ad6cb1a79c2da4e9f212d903c3d018
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2025-03-08 03:49:33 +0200

    Add menu elements

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,10 +1,34 @@
 #include <menu.hpp>
 
 #include <raylib.h>
+#include <raygui.h>
 
 #include <utilities.hpp>
 
 GameState menu()
 {
-	return MENU;
+	int activeOption;
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+			ClearBackground(GetColor(GuiGetStyle(DEFAULT, BASE_COLOR_NORMAL)));
+
+			// Title
+			DrawTextEx(GetFontDefault(), "Aperture\nNotepad", Vector2{100, 60}, 70, 6, WHITE);
+
+			// Menu options
+			GuiListView(Rectangle{ 100, 240, 330, 200 }, "Notes;Settings;Quit", nullptr, &activeOption); // Pass null to scroll index since we don't use it
+
+		EndDrawing();
+
+		switch (activeOption)
+		{
+		case 0: return SELECT_LESSONS;
+		case 1: return SETTINGS;
+		case 2: return QUIT;
+		}
+	}
+
+	return QUIT;
 }
