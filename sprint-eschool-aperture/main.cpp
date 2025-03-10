e5e06bdfe3370ef05193f40bbb6f1ac78b735625
Петър Ангелов Димитров <padimitrov23@codingburgas.bg>
2025-03-10 21:36:18 +0200

    Add redesign (Close #15)(Close #14)(Close #13)

--- sprint-eschool-aperture/main.cpp
+++ sprint-eschool-aperture/main.cpp
@@ -10,17 +10,29 @@ int main()
 		return res;
 	});
 
-	CROW_ROUTE(app, "/home.css")([]() {
+	CROW_ROUTE(app, "/css/home.css")([]() {
 		crow::response res;
-		res.set_static_file_info("static/home.css");
+		res.set_static_file_info("static/css/home.css");
 		return res;
 	});
 
 	CROW_ROUTE(app, "/about.html")([]() {
 		crow::response res;
-		res.set_static_file_info("static/about.html");
+		res.set_static_file_info("static/html/about.html");
 		return res;
 	});
+
+	CROW_ROUTE(app, "/media/fonts/Gefika.otf")([]() {
+		crow::response res;
+		res.set_static_file_info("static/media/fonts/Gefika.otf");
+		return res;
+		});
+
+	CROW_ROUTE(app, "/media/videos/backgroundVideo.mp4")([]() {
+		crow::response res;
+		res.set_static_file_info("static/media/videos/backgroundVideo.mp4"); 
+		return res;
+		});
 
 	app.multithreaded().run();
 }
