27365e9426920312a5af549a3ed3ba5567abb9e4
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2025-03-16 21:48:25 +0200

    Add error messages to registration

--- sprint-eschool-aperture/main.cpp
+++ sprint-eschool-aperture/main.cpp
@@ -20,17 +20,34 @@ int main()
 	});
 
 	CROW_ROUTE(app, "/login").methods("GET"_method)([]() {
-		crow::response response;
-		response.set_static_file_info("static/html/login.html");
-		return response;
+		return crow::mustache::load("login.html").render();
+	});
+	CROW_ROUTE(app, "/register").methods("GET"_method)([]() {
+		return crow::mustache::load("login.html").render();
 	});
 
 	// Register
+	CROW_ROUTE(app, "/register").methods("POST"_method)([&database](crow::request request) {
+		crow::response response;
+
+		if (database.insertUser(request.get_body_params().get("email"), request.get_body_params().get("password")))
+		{
+			response.set_header("Location", "/lessons");
+			response.code = crow::FOUND;
+		}
+		else
+		{
+			crow::mustache::template_t page = crow::mustache::load("login.html");
+			response = page.render({ {"emailError", true}, {"emailExists", true} });
+			response.code = crow::CONFLICT;
+		}
+
+		return response;
+	});
+
+	// Log in
 	CROW_ROUTE(app, "/login").methods("POST"_method)([&database](crow::request request) {
 		crow::response response;
-		database.insertUser(request.get_body_params().get("email"), request.get_body_params().get("password"));
-		response.code = 302;
-		response.set_header("Location", "/lessons");
 		return response;
 	});
 
