a51048bf3edf25636f66bf05b4257ac7bcd9e623
MSDimitrov23 <MSDimitrov23@codingburgas.bg>
2025-03-19 05:10:04 +0200

    Add lessons route

--- sprint-eschool-aperture/lessons.cpp
+++ sprint-eschool-aperture/lessons.cpp
@@ -1,17 +1,23 @@
 #include "lessons.hpp"
 
 #include <crow.h>
-#include <crow/middlewares/session.h>
 
 #include "app.hpp"
+#include "authentication.hpp"
 
 void setupLessons(App& app, crow::Blueprint& lessonsBlueprint)
 {
 	if (lessonsBlueprint.prefix() != "lessons")
 		lessonsBlueprint = crow::Blueprint("lessons");
-	crow::SessionMiddleware
-	CROW_BP_ROUTE(lessonsBlueprint, "/")([]()
+
+	lessonsBlueprint.CROW_MIDDLEWARES(app, Authentication);
+
+	CROW_BP_ROUTE(lessonsBlueprint, "")([]()
 	{
-		;
+		crow::response response;
+		response.set_static_file_info("static/html/lessons.html");
+		return response;
 	});
+
+	app.register_blueprint(lessonsBlueprint);
 }
