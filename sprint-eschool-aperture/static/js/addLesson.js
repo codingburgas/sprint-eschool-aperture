a83d64668195b6e7ae5460e59a590c2c9a3da974
Петър Ангелов Димитров <padimitrov23@codingburgas.bg>
2025-03-22 19:01:41 +0200

    Add get fileName js

---
+++ sprint-eschool-aperture/static/js/addLesson.js
@@ -0,0 +1,9 @@
+const addLessonsButton = document.getElementById("add");
+const lessonList = document.getElementById("lesson-list");
+
+addLessonsButton.addEventListener("click", () => {
+
+const fileName = prompt("Name your file:");
+
+
+});
